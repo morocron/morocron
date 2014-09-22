344295861321b1dc896ae41685cb0a79779763e7
Abdoul N'Diaye <wn-a.ndiaye@lagardere-active.com>
2014-09-22 11:03:56 +0200

    Add morocron application

---
+++ Application/MorocronApplication.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Morocron project.
+ *
+ * (c) Benoit Maziere <ldf-b.maziere@lagardere-active.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Morocron\Application;
+
+use Symfony\Component\Console\Application as AbstractApplication;
+
+/**
+ * Class MorocronApplication
+ * @package Morocron\Application
+ * @author Abdoul N'Diaye <abdoul.nd@gmail.com>
+ */
+class MorocronApplication extends AbstractApplication
+{
+
+}
---
+++ morocron.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Morocron project.
+ *
+ * (c) Benoit Maziere <ldf-b.maziere@lagardere-active.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Morocron\Application\MorocronApplication as Application;
+
+$application = new Application();
+$application->run();

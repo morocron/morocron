85cf20fb98713b07731c2af0de668f2b75fc7b97
Abdoul N'Diaye <wn-a.ndiaye@lagardere-active.com>
2014-09-22 17:44:41 +0200

    Remove symfony/finder from composer.json

---
+++ Cron/CronTabDefinition.php
@@ -0,0 +1,22 @@
+<?php
+
+ /**
+ * This file is part of the Europe1 project.
+ *
+ * (c) Europe1
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Morocron\Cron;
+
+/**
+ * Class CronTabDefinition
+ * @package Morocron\Cron
+ * @author Abdoul N'Diaye <wn-a.ndiaye@lagardere-active.com>
+ */
+class CronTabDefinition
+{
+
+}
